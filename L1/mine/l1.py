60ea2697e2b06072a0d2f64a6901aa8f92a0c16a
oleksandr.katrusha <oleksandr.katrusha@evologics.de>
2024-10-19 22:48:14 +0300

    l1 - node probabilities ok

--- L1/mine/l1.py
+++ L1/mine/l1.py
@@ -1,6 +1,49 @@
 import json
 from l1animal import topology, data 
-import l1asia
+#from l1asia import topology, data 
 
-for a in data:
-    print(data[a])
+parents = {root for root, child in topology}
+children = {child for root, child in topology}
+# Select roots that are not children
+roots = parents - children
+parsed_nodes = {}
+parsed_nodes_full = {}
+
+def parse_node(node, parent):
+    global parsed_nodes
+    if node not in parsed_nodes:
+        parsed_nodes[node] = {}
+        for state in data[node]:                
+            state_probability = 0
+            for parent_state in data[node][state]:
+                state_probability += parsed_nodes[parent][parent_state] * data[node][state][parent_state]
+            parsed_nodes[node][state] = state_probability        
+
+    for c in [child for parent, child in topology if parent == node]:
+        parse_node(c, node)            
+
+for r in roots:
+    states = {}
+    for s in data[r]:
+        states[s] = data[r][s]
+    parsed_nodes[r] = states
+        
+    parse_node(r, None)
+
+# print("Nodes probabilities >>>>>>>>>>>>>>>")
+# for pn in parsed_nodes:
+#     for s in parsed_nodes[pn]:
+#         print("Node: {}, State: {}, probability: {}".format(pn, s, parsed_nodes[pn][s]))
+# print("<<<<<<<<<<<<<<< Nodes probabilities")
+
+print("Nodes probabilities >>>>>>>>>>>>>>>")
+for pn in parsed_nodes:
+    for s in parsed_nodes[pn]:
+        print("Node: {}, State: {}, probability: {}".format(pn, s, parsed_nodes[pn][s]))
+print("<<<<<<<<<<<<<<< Nodes probabilities")
+
+# all_states = []
+# for pn in parsed_nodes:
+#     for s in parsed_nodes[pn]:
+#         all_states.append((pn, s))
+
--- L1/mine/l1animal.py
+++ L1/mine/l1animal.py
@@ -2,52 +2,121 @@ topology = [('Animal', 'Environment'),
                    ('Animal', 'HasShell'),
                    ('Animal', 'Class'),
                    ('Animal', 'BearsYoungAs'),
-                   ('Class', 'WarmBllooded'),
+                   ('Class', 'WarmBlooded'),
                    ('Class', 'BodyCovering')]
 data = {
         "Animal": {
-            "Monkey": [0, 2],
-            "Penguin": [0, 2],
-            "Platypus": [0, 2],
-            "Robin": [0, 2],
-            "Turtle": [0, 2]
+            "Monkey": 0.2,
+            "Penguin": 0.2,
+            "Platypus": 0.2,
+            "Robin": 0.2,
+            "Turtle": 0.2
         },
         "Environment": {
-            "Monkey": {"Air": 0, "Land": 1, "Water": 0},
-            "Penguin": {"Air": 0, "Land": 0.5, "Water": 0.5},
-            "Platypus": {"Air": 0, "Land": 0, "Water": 1},
-            "Robin": {"Air": 0.5, "Land": 0.5, "Water": 0},
-            "Turtle": {"Air": 0, "Land": 0.5, "Water": 0.5}
+            "Air": {
+                "Monkey": 0,
+                "Penguin": 0,
+                "Platypus": 0,
+                "Robin": 0.5,
+                "Turtle": 0
+            },
+            "Land": {
+                "Monkey": 1,
+                "Penguin": 0.5,
+                "Platypus": 0,
+                "Robin": 0.5,
+                "Turtle": 0.5
+            },
+            "Water": {
+                "Monkey": 0,
+                "Penguin": 0.5,
+                "Platypus": 1,
+                "Robin": 0,
+                "Turtle": 0.5
+            }
         },
         "HasShell": {
-            "Monkey": {"True": 0, "False": 1},
-            "Penguin": {"True": 0, "False": 1},
-            "Platypus": {"True": 0, "False": 1},
-            "Robin": {"True": 0, "False": 1},
-            "Turtle": {"True": 1, "False": 0}
+            "True": {
+                "Monkey": 0,
+                "Penguin": 0,
+                "Platypus": 0,
+                "Robin": 0,
+                "Turtle": 1
+            },
+            "False": {
+                "Monkey": 1,
+                "Penguin": 1,
+                "Platypus": 1,
+                "Robin": 1,
+                "Turtle": 0
+            }
         },
         "BearsYoungAs": {
-            "Monkey": {"Live": 1, "Eggs": 0},
-            "Penguin": {"Live": 0, "Eggs": 1},
-            "Platypus": {"Live": 0, "Eggs": 1},
-            "Robin": {"Live": 0, "Eggs": 1},
-            "Turtle": {"Live": 0, "Eggs": 1}
+            "Live": {
+                "Monkey": 1,
+                "Penguin": 0,
+                "Platypus": 0,
+                "Robin": 0,
+                "Turtle": 0
+            },
+            "Eggs": {
+                "Monkey": 0,
+                "Penguin": 1,
+                "Platypus": 1,
+                "Robin": 1,
+                "Turtle": 1
+            }
         },
         "Class": {
-            "Monkey": {"Bird": 0, "Mammal": 1, "Reptile": 0},
-            "Penguin": {"Bird": 1, "Mammal": 0, "Reptile": 0},
-            "Platypus": {"Bird": 0, "Mammal": 1, "Reptile": 0},
-            "Robin": {"Bird": 1, "Mammal": 0, "Reptile": 0},
-            "Turtle": {"Bird": 0, "Mammal": 0, "Reptile": 1}
+            "Bird": {
+                "Monkey": 0,
+                "Penguin": 1,
+                "Platypus": 0,
+                "Robin": 1,
+                "Turtle": 0
+            },
+            "Mammal": {
+                "Monkey": 1,
+                "Penguin": 0,
+                "Platypus": 1,
+                "Robin": 0,
+                "Turtle": 0
+            },
+            "Reptile": {
+                "Monkey": 0,
+                "Penguin": 0,
+                "Platypus": 0,
+                "Robin": 0,
+                "Turtle": 1
+            }
         },
         "WarmBlooded": {
-            "Bird": {"True": 1, "False": 0},
-            "Mammal": {"True": 1, "False": 0},
-            "Reptile": {"True": 0, "False": 1}
+            "True": {
+                "Bird": 1,
+                "Mammal": 1,
+                "Reptile": 0
+            },
+            "False": {
+                "Bird": 0,
+                "Mammal": 0,
+                "Reptile": 1
+            }
         },
         "BodyCovering": {
-            "Bird": {"Fur": 0, "Feathers": 1, "Scales": 0},
-            "Mammal": {"Fur": 1, "Feathers": 0, "Scales": 0},
-            "Reptile": {"Fur": 0, "Feathers": 0, "Scales": 1}
+            "Fur": {
+                "Bird": 0,
+                "Mammal": 1,
+                "Reptile": 0
+            },
+            "Feathers": {
+                "Bird": 1,
+                "Mammal": 0,
+                "Reptile": 0
+            },
+            "Scales": {
+                "Bird": 0,
+                "Mammal": 0,
+                "Reptile": 1
+            }
         }
 }
